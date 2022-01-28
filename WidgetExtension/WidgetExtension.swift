7159b2e9bc7ab701225a2ef749821bc5a178d8ac
ChangMin <sopg04@gmail.com>
2022-01-28 22:12:20 +0900

    사이즈 및 설명 추가

--- WidgetExtension/WidgetExtension.swift
+++ WidgetExtension/WidgetExtension.swift
@@ -10,19 +10,22 @@ import SwiftUI
 import Intents
 
 struct Provider: IntentTimelineProvider {
+    // 갱신 전에 디폴트로 값을 보여주는 부분
     func placeholder(in context: Context) -> SimpleEntry {
         SimpleEntry(date: Date(), configuration: ConfigurationIntent())
     }
 
+    // 설정에서의 프리뷰 화면
     func getSnapshot(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (SimpleEntry) -> ()) {
         let entry = SimpleEntry(date: Date(), configuration: configuration)
         completion(entry)
     }
 
+    // TimelineEntry에 들어있는 값을 표현해주는 부분
     func getTimeline(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
         var entries: [SimpleEntry] = []
 
-        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
+       
         let currentDate = Date()
         for hourOffset in 0 ..< 5 {
             let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
@@ -30,6 +33,7 @@ struct Provider: IntentTimelineProvider {
             entries.append(entry)
         }
 
+        // policy에 따라 entries 값을 가지고 리로드 해주는 부분
         let timeline = Timeline(entries: entries, policy: .atEnd)
         completion(timeline)
     }
@@ -40,6 +44,7 @@ struct SimpleEntry: TimelineEntry {
     let configuration: ConfigurationIntent
 }
 
+// 실제 위젯을 표현하는 부분
 struct WidgetExtensionEntryView : View {
     var entry: Provider.Entry
 
@@ -59,6 +64,7 @@ struct WidgetExtensionEntryView : View {
 struct WidgetExtension: Widget {
     let kind: String = "WidgetExtension"
 
+    // 위젯 설정에서 설정하는 부분
     var body: some WidgetConfiguration {
         IntentConfiguration(kind: kind, intent: ConfigurationIntent.self, provider: Provider()) { entry in
             WidgetExtensionEntryView(entry: entry)
